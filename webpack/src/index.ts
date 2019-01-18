ef48824a17dcc0c3af7243b1aa5813bdeae843d0
Rafael Porras <rporrasdominguez@eresmas.com>
2019-01-18 18:53:24 +0100

    solucionado problema con typescript

--- webpack/src/index.ts
+++ webpack/src/index.ts
@@ -5,14 +5,18 @@ var logo = require('./content/logo_1.png');
 
 const img = document.createElement('img');
 img.src = logo;
-img.id='logo';
+img.id ='logo';
 document.getElementById('imageContainer').appendChild(img);
 
 
-const numbers = [1,3,4,5,7];
+const numbers : number []= [1,3,4,5,7];
 
-let [firstPosition] = numbers;
+let [firstPosition]  = numbers;
 
 console.log(firstPosition);
+
+const sentence :String = 'Esto es una frase chorra';
+
+console.log(sentence);
 
 
