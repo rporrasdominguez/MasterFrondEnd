55d721bd684921bcbe7a80df911f952b69e1fdb5
Rafael Porras <rporrasdominguez@eresmas.com>
2019-01-18 17:31:45 +0100

    configuracion de css loader y css loader

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -6,7 +6,7 @@ var basePath = __dirname;
 module.exports = {
     context: path.join(basePath,'src'), //añade a la ruta base la carpeta src
 
-    entry:['@babel/polyfill','./index.js','./'], //ficheros de entrada
+    entry:['@babel/polyfill','./index.js','./style.css'], //ficheros de entrada
 
     output:{filename:'bundle.js'}, //fichero de salida
 
@@ -15,7 +15,21 @@ module.exports = {
             test:/\.js$/,
             exclude:/node_modules/,
             loader:'babel-loader', //loader de babel
-        }],
+        },
+        {
+            test: /\.css$/, //analiza los ficheros css
+            exclude: /node_modules/, //no mires en la carpeta node_modules
+            use:[ //al usar mas de un loader usaremos use y en su interior los distintos loader
+                {
+                    loader:'style-loader', //Nos coge los estilos del js bundle y no incrusta en la cabecera del html
+                  
+                },
+                {
+                    loader: 'css-loader', //loader que maneje el css y lo introduce en el js
+                },
+            ] 
+        },
+    ],
     },
 
     plugins:[ //area de plugins
