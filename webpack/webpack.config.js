77ce867514fba09fc84f40c111d786e826d66232
Rafael Porras <rporrasdominguez@eresmas.com>
2019-01-18 17:23:03 +0100

    creacion de un fichero css

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,18 +1,29 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
 var path = require('path');
 var basePath = __dirname;
 
 module.exports = {
     context: path.join(basePath,'src'), //añade a la ruta base la carpeta src
 
-    entry:['@babel/polyfill','./index.js'],
+    entry:['@babel/polyfill','./index.js'], //ficheros de entrada
 
-    output:{filename:'bundle.js'},
+    output:{filename:'bundle.js'}, //fichero de salida
 
     module:{
         rules:[{
             test:/\.js$/,
             exclude:/node_modules/,
-            loader:'babel-loader',
+            loader:'babel-loader', //loader de babel
         }],
     },
+
+    plugins:[ //area de plugins
+        new HtmlWebpackPlugin({ //cargamos el plugin htmlwebpackplugin
+            filename:'index.html', //fichero de destino que se colocara en ./dist
+            template: 'index.html', //fichero de origen en ./
+            hash: true, //Con lo que forzaremos al navegador a refrescar con cada nuevo cambio de la pagina
+        }),
+
+    ]
 }
