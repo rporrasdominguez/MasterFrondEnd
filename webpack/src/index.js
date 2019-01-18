836eb148cfc62cf19e5e25a285ab99a464217ea1
Rafael Porras <rporrasdominguez@eresmas.com>
2019-01-18 17:58:54 +0100

    Añadimos el logo mediante js

--- webpack/src/index.js
+++ webpack/src/index.js
@@ -1,5 +1,15 @@
+import logo from './content/logo_1.png';
+
+
+const img = document.createElement('img');
+img.src = logo;
+document.getElementById('imgContainer').appendChild(img);
+
+
 const numbers = [1,3,4,5,7];
 
 let [firstPosition] = numbers;
 
 console.log(firstPosition);
+
+
