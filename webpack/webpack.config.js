1abdca57091a573c1162e921ed45a548d8f0f66a
Rafael Porras <rporrasdominguez@eresmas.com>
2019-01-18 17:49:40 +0100

    configuracion sass-loader y node-sass

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
         app:'./index.js', //Carpeta donde se guardaran los js, siendo index.js el fichero principal
 
         appStyles:[
-            './style.css', //Carpeta donde se guardaran los css, siendo style el fichero principal
+            './style.scss', //Carpeta donde se guardaran los css, siendo style el fichero principal
         ],
         vendor:[
             '@babel/polyfill', //carga los @babel/polyfill
@@ -40,6 +40,7 @@ module.exports = {
             exclude:/node_modules/,
             loader:'babel-loader', //loader de babel
         },
+
         {
             test: /\.css$/, //analiza los ficheros css
             exclude: /node_modules/, //no mires en la carpeta node_modules
@@ -50,6 +51,17 @@ module.exports = {
                 {
                     loader: 'css-loader', //loader que maneje el css y lo introduce en el js
                 },
+            ] 
+        },
+
+        {
+            test: /\.scss$/, //analiza los ficheros scss
+            exclude: /node_modules/, //no mires en la carpeta node_modules
+            use:[ 
+
+                    MiniCssExtractPlugin.loader, //sin comillas ya que es una variable
+                    'css-loader', //loader que maneje el css y lo introduce en el js
+                    'sass-loader',//loader para que el css lo introduzca en el js  
             ] 
         },
     ],
