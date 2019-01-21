9004776d2811dba9271d218f503eae150d17ff64
Rafael Porras <rporrasdominguez@eresmas.com>
2019-01-21 16:07:00 +0100

    configuracion fichero webpack produccion

--- webpack/prod.webpack.config.js
+++ webpack/prod.webpack.config.js
@@ -1,6 +1,16 @@
 const merge = require('webpack-merge');
 const common = require('./base.webpack.config.js');
+const CompressionPlugin = require('compression-webpack-plugin');
 
 module.exports = merge(common, {
     mode: 'production',
+    plugins: [
+        new CompressionPlugin({
+            filename: '[path].gz[query]',
+            algorithm: 'gzip',
+            test: /\.js$|\.jsx$|\.scss$|\.css$|\.html$/,
+            threshold: 1024,
+            minRatio: 0.8
+        }),
+    ],
 });
