9567a92214b71c9b979d41f674fc3ae03fa3717a
Rafael Porras <rporrasdominguez@eresmas.com>
2019-01-22 13:05:39 +0100

    configuracion de rimraf

--- parcel/dist/src.e31bb0bc.js
+++ parcel/dist/src.e31bb0bc.js
@@ -133,7 +133,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "41343" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "44113" + '/');
 
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
