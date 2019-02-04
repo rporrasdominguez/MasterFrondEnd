4bc76fd8747f341a448bac7bdb136cca461a3e5d
Rafael Porras <rporrasdominguez@eresmas.com>
2019-02-04 11:47:40 +0100

    configurarmos el input type text

--- redux/00_start/src/components/memberList/memberArea.tsx
+++ redux/00_start/src/components/memberList/memberArea.tsx
@@ -5,19 +5,21 @@ import {MemberEntity} from '../../model/member'
 interface Props {
   members: Array<MemberEntity>;
   company: string,
-  loadMembers: () => any;
-  onChangeCompany: () => void;
+  loadMembers: (company:string) => any;
+  onChangeCompany: (company:string) => void;
 }
 
 export const MemberAreaComponent = (props : Props) => {
   return (
   <div>
       <input type='text'
+             onChange={(e) => props.onChangeCompany(e.target.value)}
+             value={props.company}
       />
       <input type="submit"
               value="load"
               className="btn btn-default"
-              onClick={() => props.loadMembers()}
+              onClick={() => props.loadMembers(props.company)}
       />
       <MemberTableComponent members={props.members}/>
    
