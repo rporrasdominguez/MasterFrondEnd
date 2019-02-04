f5c04ba732e34708dfa9a0979b33b4a17ee24dd5
Rafael Porras <rporrasdominguez@eresmas.com>
2019-02-04 12:02:50 +0100

    Modificamos reducer añadiendo un nuevo tipo, company, una interface baseAction y una funcion que crea stado por defecto

--- redux/00_start/src/reducers/memberReducer.ts
+++ redux/00_start/src/reducers/memberReducer.ts
@@ -1,7 +1,20 @@
 import {actionsEnums} from '../common/actionsEnums';
 import {MemberEntity} from '../model/member';
 
-export type memberState =  MemberEntity[];
+export type memberState = {
+  members: MemberEntity[];
+  company:string;
+}
+
+interface BaseAction{
+  type:string;
+  payload:any;
+}
+
+const createDefaultState = (): memberState =>({
+  members: [],
+  company: 'lemoncode',
+})
 
 export const memberReducer =  (state : memberState = [], action) => {
   switch (action.type) {
